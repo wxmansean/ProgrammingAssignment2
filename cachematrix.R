2be8b4bd9d823c17d222e2e02172eb6bf345791c
wxmansean <wxmansean@gmail.com>
2015-02-20 17:19:55 -0500

    Updated with my solution to the problem

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,50 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Coursera  Programming in R, Assignment #2
+##
+## Author: Sean Downey February 20, 2015
+## 
+##
+## Below are two functions that are used to create a special object that stores a matrix and caches its inverse.
 
-## Write a short comment describing this function
+## This function sets up a matrix, storing it in anothher environment
+## it also sets up 'setters' and 'getters' for doing the inverse calculations
 
 makeCacheMatrix <- function(x = matrix()) {
+  
+  m <- NULL
 
+  ## this acts to cache the matrix in a seperate environment
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+
+  ## set up the matrix-related functions
+  get <- function() x
+  setInverse <- function(solve) m <<- solve
+  getInverse <- function() m
+  list(set = set, get = get,
+       setInverse = setInverse,
+       getInverse = getInverse)
 }
 
 
-## Write a short comment describing this function
+## This function will, when called, check to see if the inversae calculation was already done. 
+## if the calculation has already been down, we use the cached result, otherwise we do the calculation
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  
+  ## attempt to get the calculated matrix
+  m <- x$getInverse()
+  
+  ## if we already did it, return those results, we're done
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  
+  # haven;t done anything, we need to do the calculations 
+  data <- x$get()
+  m <- solve(data, ...)
+  x$setInverse(m)
+  m
 }
